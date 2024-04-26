d01cf7717dff33db4ef855811444248d5b04f4b7
ekamleen <ekamleenm@gmail.com>
2024-04-26 13:49:46 -0700

    GUI register/login

--- App.py
+++ App.py
@@ -14,6 +14,9 @@ class NLPApp:
         self.root.mainloop()
 
     def login_gui(self):
+
+        self.clear()
+
         heading = Label(self.root, text='NLPApp', bg='#93879c', fg='white')
         heading.pack(pady=(30, 30))
         heading.configure(font=('verdana', 24, 'bold'))
@@ -29,7 +32,7 @@ class NLPApp:
         label2.pack(pady=(10, 10))
         label2.configure(font=('verdana', 17, 'bold'))
 
-        self.password_input = Entry(self.root, width=30)
+        self.password_input = Entry(self.root, width=30, show='*')
         self.password_input.pack(pady=(5, 10))
 
         login_button = Button(self.root, text='login', width=20, height=1)
@@ -39,9 +42,52 @@ class NLPApp:
         label3.pack(pady=(20, 10))
         label3.configure(font=('verdana', 14, 'bold'))
 
-        redirect_button = Button(self.root, text='Register Now', width=16, height=1)
+        redirect_button = Button(self.root, text='Register Now', width=16, height=1, command=self.register_gui)
         redirect_button.pack(pady=(10, 10))
+
+    def register_gui(self):
+        # Clear the existing GUI
+        self.clear()
+
+        heading = Label(self.root, text='Welcome to NLPApp', bg='#93879c', fg='white')
+        heading.pack(pady=(30, 30))
+        heading.configure(font=('verdana', 24, 'bold'))
+
+        label0 = Label(self.root, text='Enter Name: ', bg='#93879c', fg='white')
+        label0.pack(pady=(10, 10))
+        label0.configure(font=('verdana', 17, 'bold'))
+
+        self.name_input = Entry(self.root, width=30)
+        self.name_input.pack(pady=(5, 10))
+
+        label1 = Label(self.root, text='Enter Email: ', bg='#93879c', fg='white')
+        label1.pack(pady=(10, 10))
+        label1.configure(font=('verdana', 17, 'bold'))
+
+        self.email_input = Entry(self.root, width=30)
+        self.email_input.pack(pady=(5, 10))
+
+        label2 = Label(self.root, text='Enter Password: ', bg='#93879c', fg='white')
+        label2.pack(pady=(10, 10))
+        label2.configure(font=('verdana', 17, 'bold'))
+
+        self.password_input = Entry(self.root, width=30, show='*')
+        self.password_input.pack(pady=(5, 10))
+
+        login_button = Button(self.root, text='Register', width=20, height=1)
+        login_button.pack(pady=(20, 10))
+
+        label3 = Label(self.root, text='Already a Member?', bg='#93879c', fg='white')
+        label3.pack(pady=(20, 5))
+        label3.configure(font=('verdana', 14, 'bold'))
+
+        redirect_button = Button(self.root, text='Login Now', width=16, height=1, command=self.login_gui)
+        redirect_button.pack(pady=(10, 10))
+
+    def clear(self):
+        for i in self.root.pack_slaves():
+            print(i.destroy())
+
 
 
 nlp = NLPApp()
-
